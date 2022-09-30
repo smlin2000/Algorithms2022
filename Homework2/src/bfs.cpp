27b53ece526d6ddbe5e46e4b45d2e3faabc3215e
smlin2000 <smlin2000@gmail.com>
2022-09-30 13:24:31 -0500

    a

--- Homework2/src/bfs.cpp
+++ Homework2/src/bfs.cpp
@@ -4,15 +4,25 @@ void bfs(Graph &G, int start, int destination) {
     Queue<int> queue;
     G.reset();
     // YOUR CODE HERE
-    
+    queue.push(start);
+    G.setVisited(start);
     while (!queue.empty()) {
         int u = queue.pop();
         // YOUR CODE HERE
+        if (u == destination){
+            break;
+        }
         int numberOfAdjacencyNodes = G.e[u].size();
         LinkedListNode<int> *p = G.e[u].getRoot();
         for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) {
             int v = p->value;
             // YOUR CODE HERE
+            if (G.isVisited(v)){
+                continue;
+            }
+            queue.push(v);
+            G.setVisited(v);
+            G.setTrace(u, v);
         }
     }
 }
