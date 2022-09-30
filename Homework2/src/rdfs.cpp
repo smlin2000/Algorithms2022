d538552f786ea2be234df8932d780ae6ee14cb58
smlin2000 <smlin2000@gmail.com>
2022-09-30 14:12:08 -0500

    a

--- Homework2/src/rdfs.cpp
+++ Homework2/src/rdfs.cpp
@@ -2,10 +2,20 @@
 
 void rdfs(Graph &G, int start, int destination) {
     // YOUR CODE HERE
+    if (start == destination){
+        return;
+    }
+    G.setVisited(start);
+
     int numberOfAdjacencyNodes = G.e[start].size();
     LinkedListNode<int> *p = G.e[start].getRoot();
     for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) {
         int v = p->value;
         // YOUR CODE HERE
+        if (G.isVisited(v)){
+            continue;
+        }
+        G.setTrace(v, start);
+        rdfs(G, v, destination);
     }
 }
