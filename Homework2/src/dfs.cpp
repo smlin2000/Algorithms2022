6ab20b6b24ecd89822a2b06fd6f54dd2b07d3337
smlin2000 <smlin2000@gmail.com>
2022-09-30 14:01:46 -0500

    a

--- Homework2/src/dfs.cpp
+++ Homework2/src/dfs.cpp
@@ -4,14 +4,25 @@ void dfs(Graph &G, int start, int destination) {
     Stack<int> stack;
     G.reset();
     // YOUR CODE HERE
+    stack.push(start);
+    G.setVisited(start);
     while (!stack.empty()) {
         int u = stack.pop();
         // YOUR CODE HERE
+        if (u == destination){
+            break;
+        }
         int numberOfAdjacencyNodes = G.e[u].size();
         LinkedListNode<int> *p = G.e[u].getRoot();
         for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) {
             int v = p->value;
-            // YOUR CODE HERE 
+            // YOUR CODE HERE
+            if (G.isVisited(v)){
+                continue;
+            }
+            stack.push(v);
+            G.setVisited(v);
+            G.setTrace(u, v);
         }
     }
 }
