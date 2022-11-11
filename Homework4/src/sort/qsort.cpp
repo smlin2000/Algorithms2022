44ac51a624123a8494d0603f6711fd75a6491dc5
smlin2000 <smlin2000@gmail.com>
2022-11-11 16:01:37 -0600

    a

--- Homework4/src/sort/qsort.cpp
+++ Homework4/src/sort/qsort.cpp
@@ -10,7 +10,7 @@ void sort(std::vector<T> &array, int l, int r) {
         return;
     }
     int pivot = (l + r) / 2;
-    std::vector<T> &array1 = new std::vector<T>();
+    std::vector<T> array1;
     for (int i = l; i <= pivot; i++){
         array1.push_back(array[i]);
     }
