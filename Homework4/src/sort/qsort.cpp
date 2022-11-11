5e0afe0d933b8b42697772b8408f2d3b06b48959
smlin2000 <smlin2000@gmail.com>
2022-11-11 15:50:06 -0600

    a

--- Homework4/src/sort/qsort.cpp
+++ Homework4/src/sort/qsort.cpp
@@ -7,12 +7,12 @@ std::string sortAlgName = "Quick Sort";
 template<class T>
 void sort(std::vector<T> &array, int l, int r) {
     if (l >= r){
-        return
+        return;
     }
-    int pivot = (l + r) / 2
-    vector<T>::const_iterator first = array.begin();
-    vector<T>::const_iterator last = array.begin() + pivot;
-    std::vector<T> &array1 = newVec(first, last)
+    int pivot = (l + r) / 2;
+    std::vector<T>::const_iterator first = array.begin();
+    std::vector<T>::const_iterator last = array.begin() + pivot;
+    std::vector<T> &array1 = newVec(first, last);
 }
 
 template void sort(std::vector<Edge> &array,  int l, int r);
