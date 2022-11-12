643925774371d36bdefb17163df75c7e9f4d408c
smlin2000 <smlin2000@gmail.com>
2022-11-11 20:38:55 -0600

    a

--- Homework4/src/sort/hsort.cpp
+++ Homework4/src/sort/hsort.cpp
@@ -4,11 +4,60 @@
 //extern
 std::string sortAlgName = "Heap Sort";
 
+void heapify(std::vector<T> &heap, int N, int i) {
+
+  // N is the size of the current heap
+
+  // i is the index of current node
+
+ 
+
+    left = index of left child of i // which is 2*i + 1
+
+    right = index of right child of i // which is 2*i + 2
+
+   
+
+    if node has no chilren then // which mean left >= N
+
+       return;
+
+    end if
+
+  
+
+    if (node i has right child // which mean right < N
+
+            and value right child > value of left child) then
+
+        candidate = right;
+
+    else
+
+        candidate = left;
+
+   end if
+
+   
+
+    if (value of node i < value of node candidate) then
+
+        swap value of node and value of candidate
+
+        recursively call heapify(heap, N, candidate)
+
+    end if
+
+}
+
 template<class T>
 void sort(std::vector<T> &array, int l, int r) {
     int n = (r - l + 1);
-    heap
+    std::vector<T> &heap(0, n-1) = array;
+
 }
+
+
 
 template void sort(std::vector<Edge> &array,  int l, int r);
 template void sort(std::vector<int> &array,  int l, int r);
