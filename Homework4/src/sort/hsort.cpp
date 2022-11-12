3ba5f2938325ac48d7774e267e3f3ed0f07f8bd1
smlin2000 <smlin2000@gmail.com>
2022-11-11 22:14:01 -0600

    a

--- Homework4/src/sort/hsort.cpp
+++ Homework4/src/sort/hsort.cpp
@@ -71,7 +71,7 @@ void heapify(std::vector<T> &heap, int N, int i) {
 template<class T>
 void sort(std::vector<T> &array, int l, int r) {
     int n = (r - l + 1);
-    std::vector<T> &heap = new int[n];
+    std::vector<T> &heap = new T[n];
     for (int i = l; i <= r; i++){
         heap [i - l] = array[i];
     }
