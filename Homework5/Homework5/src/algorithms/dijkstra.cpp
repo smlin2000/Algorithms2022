86b546e4c83bf5f7f5a6303f8b8e278a2fcabfca
smlin2000 <smlin2000@gmail.com>
2022-12-02 20:04:54 -0600

    a

--- Homework5/Homework5/src/algorithms/dijkstra.cpp
+++ Homework5/Homework5/src/algorithms/dijkstra.cpp
@@ -14,8 +14,8 @@ std::vector<int> searchShortestPath(Graph &G, int start, int destination) {
     std::vector<int> distance(G.n, INT_MAX); // Initialize the distances of all vertex into maximum value
     std::vector<int> parent(G.n, -1); // Initialize the parent of each vertex by -1
 
-    distance[start] = ???; // Inititlize distance of start. You have to fill in here based on the pseudo code
-    parent[start] = ???; // Initialize parent of start. You have to fill in here based on the pseudo code
+    distance[start] = 0; // Inititlize distance of start. You have to fill in here based on the pseudo code
+    parent[start] = -1; // Initialize parent of start. You have to fill in here based on the pseudo code
 
 
     for (int i = 1; i <= G.n - 1; ++i) { // We loop over n-1 times as there will be not path has the number of edges > n-1
@@ -23,7 +23,7 @@ std::vector<int> searchShortestPath(Graph &G, int start, int destination) {
         for (int i = 0; i < G.n; ++i)
             if (distance[i] < minDistance && visited[i] == false) // We are searching for a vertex not visited yet has the minimum distance
                 minDistance = distance[i], u = i;
-        visited[u] = ???; // Set u as a visited vertex. You have to fill in here.
+        visited[u] = true; // Set u as a visited vertex. You have to fill in here.
 
         // Now we have to update distance start from u
         // As only neighbor vertex of u is affected, we only a neighbor of u which is not vitisted yet 
@@ -31,17 +31,17 @@ std::vector<int> searchShortestPath(Graph &G, int start, int destination) {
             int v = e.v;
             int w = e.w;
             if (visited[v] == false && distance[u] + w < distance[v]) {
-                distance[v] = ??? ; // You have to fill in here based on the pseudo code 
-                parent[v] = ???; // You have to fill in there based on the pseudo code
+                distance[v] = distance[u] + w; // You have to fill in here based on the pseudo code 
+                parent[v] = u; // You have to fill in there based on the pseudo code
             }
         }
     }
 
     std::vector<int> path;
-    int u = ???; // We trace back from the destination. You have to fill in there based on the pseudo code 
+    int u = destination; // We trace back from the destination. You have to fill in there based on the pseudo code 
     while (u != -1) {
         path.push_back(u);
-        u = ???; // Based on parent[u], we move back one step. You have to fill in here based on the pseudo code
+        u = parent[u]; // Based on parent[u], we move back one step. You have to fill in here based on the pseudo code
     }
     // After the while loop, we will have a path from destination -- to --> start, we need to reverse an array to get the correct result
     std::reverse(path.begin(), path.end());
